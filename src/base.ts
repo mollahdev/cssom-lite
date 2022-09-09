0d6447e568870cac4bff984cde0b203883e7a6b7
Ashraf Mollah <mollah.dev@gmail.com>
2022-09-09 10:10:45 +0600

    prepared for release 1.0.0

--- src/base.ts
+++ src/base.ts
@@ -1,59 +1,145 @@
-import { cssOption, devicesOption, responsiveOption, rulesOption } from "./interfaces";
+import { cssOption, devicesOption, responsiveOption, rulesOption, stringObj } from "./interfaces";
 
 export default class Base {
-    
-    protected devices   : devicesOption = {}
-    protected css       : cssOption     = {}
-    protected rules     : rulesOption   = {}
+
+    protected devices: devicesOption = {}
+    protected css: cssOption = {}
+    protected rules: rulesOption = {}
 
     protected get uid(): number {
-        return Math.floor( Math.random() * 100000 );
+        return Math.floor(Math.random() * 100000);
     }
 
-    protected sortDevices(): void | this {
+    protected sortDevices(): void | false {
 
-        const self          = this;
-        const deviceNames   = Object.keys( self.devices );
+        const self = this;
+        const deviceNames = Object.keys(self.devices);
 
-		if ( deviceNames.length < 2 ) {
-            return self;
+        if (deviceNames.length < 2) {
+            return;
         }
 
-		deviceNames.sort( ( a: string, b: string ) => {
-			return self.devices[ a ] - self.devices[ b ];
-		} );
+        deviceNames.sort((a: string, b: string) => {
+            return self.devices[a] - self.devices[b];
+        });
 
-		const sortedDevices: devicesOption = {};
+        const sortedDevices: devicesOption = {};
 
-		deviceNames.forEach( function( deviceName: string ) {
-			sortedDevices[ deviceName ] = self.devices[ deviceName ];
-		});
+        deviceNames.forEach(function (deviceName: string) {
+            sortedDevices[deviceName] = self.devices[deviceName];
+        });
 
-		self.devices = sortedDevices;
+        self.devices = sortedDevices;
     }
-
-    protected removeMultiWhiteSpace( str: string ): string {
-        if( typeof str !== 'string' ) return str;
-        return str.trim().replace(/ +(?= )/g,'');
+    /**
+     * @param  {string} str
+     * @returns string
+     */
+    protected removeMultiWhiteSpace(str: string): string {
+        if (typeof str !== 'string') return str;
+        return str.trim().replace(/ +(?= )/g, '');
     }
-
-    protected responsiveToHash( responsive: responsiveOption ): string {
-		let hash = [];
+    /**
+     * @param  {responsiveOption} responsive
+     */
+    protected responsiveToHash(responsive: responsiveOption): string {
+        let hash = [];
         let endPoint: keyof typeof responsive
-		for( endPoint in responsive ) {
-			hash.push( endPoint + '_' + responsive[endPoint] );
-		}
-		return hash.join( '-' );
-	}
-
-    protected hashToResponsive( hash: string ): devicesOption {
-        const responsive: devicesOption  = {};
-        const hashArray  = hash.split( '-' ).filter( String );
-        hashArray.forEach( hashItem => {
-            const 
-                [ endPoint, deviceName ] = hashItem.split( /_(.+)/ );
-                responsive[ endPoint ]  = this.devices[ deviceName ]
-        } );
+        for (endPoint in responsive) {
+            hash.push(endPoint + '_' + responsive[endPoint]);
+        }
+        return hash.join('-');
+    }
+    /**
+     * @param  {string} hash
+     */
+    protected hashToResponsive(hash: string): devicesOption {
+        const responsive: devicesOption = {};
+        const hashArray = hash.split('-').filter(String);
+        hashArray.forEach(hashItem => {
+            const
+                [endPoint, deviceName] = hashItem.split(/_(.+)/);
+            responsive[endPoint] = this.devices[deviceName]
+        });
         return responsive;
+    }
+
+    protected sortHashes(): rulesOption {
+
+        const self = this, { rules, hashToResponsive } = self;
+
+        const hashes = Object.keys(rules);
+        if (hashes.length < 2) return rules;
+
+        hashes.sort(function (a, b) {
+            if ('all' === a) {
+                return -1;
+            }
+
+            if ('all' === b) {
+                return 1;
+            }
+
+            let aQuery = hashToResponsive.call(self, a),
+                bQuery = hashToResponsive.call(self, b);
+
+            if (aQuery.max && bQuery.max) {
+                return bQuery.max - aQuery.max;
+            }
+
+            if (aQuery.min && bQuery.min) {
+                return bQuery.min - aQuery.min;
+            }
+
+            const aQueryValue = aQuery.max ?? aQuery.min;
+            const bQueryValue = bQuery.max ?? bQuery.min;
+
+            return bQueryValue - aQueryValue;
+        });
+
+        const sortedRules: rulesOption = {};
+        hashes.forEach(deviceName => {
+            sortedRules[deviceName] = rules[deviceName];
+        });
+
+        return sortedRules;
+    }
+    /**
+     * @param  {string} responsiveHash
+     */
+    createResponsiveFormat(responsiveHash: string) {
+        const responsive = this.hashToResponsive.call(this, responsiveHash)
+        const responsiveFormat = [];
+
+        for (let endPoint in responsive) {
+            responsiveFormat.push('(' + endPoint + '-width:' + responsive[endPoint] + 'px)');
+        }
+
+        return '@media' + responsiveFormat.join(' and ');
+    }
+
+    /**
+     * @param  {stringObj} properties
+     * @returns string
+     */
+    convertProperties(properties: stringObj): string {
+        let convertedProperties = '';
+        for (let property in properties) {
+            convertedProperties += property + ':' + properties[property] + ';';
+        }
+        return convertedProperties;
+    }
+    /**
+     * @param  {{[key:string]:{[key:string]:string}}} rules
+     */
+    convertRules(rules: { [key: string]: { [key: string]: string } }) {
+        let convertedRules = '';
+        for (let selector in rules) {
+            const convertedProperties = this.convertProperties(rules[selector]);
+            if (convertedProperties) {
+                convertedRules += selector + '{' + convertedProperties + '}';
+            }
+        }
+        return convertedRules;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,107 @@
 import Base from "./base";
-import { responsiveOption, rulesOption, stringObj } from "./interfaces";
-
-class CSSMaker extends Base {
-    
-    public addDevice( deviceName: string, maxPoint: number ): void {
-        this.devices[deviceName] = maxPoint;
-        this.sortDevices();
-    }
-
-    public addCSS( content: string ): void {
-		this.css[this.uid] = content;
+import { responsiveOption, stringObj } from "./interfaces";
+export default class CSSMaker extends Base {
+	/**
+	 * @param  {string} deviceName
+	 * @param  {number} maxPoint
+	 */
+	public addDevice(deviceName: string, maxPoint: number): void {
+		this.devices[deviceName] = maxPoint;
+		this.sortDevices();
 	}
+	/**
+	 * @param  {string} content
+	 * @returns void
+	 */
+	public addCSS(content: string): void {
+		this.css[this.uid] = content
+	}
+	/**
+	 * @param  {string} selector
+	 * @param  {string} properties
+	 * @param  {responsiveOption} responsive?
+	 */
+	public addRule(selector: string, properties: string, responsive?: responsiveOption): void | false {
 
-    public addRule( selector: string, properties: string, responsive?: responsiveOption ): void | false {
-		
-        const self          = this;
-		let hash            = 'all';
-        let propertiesObj: stringObj | string[]   = {}
+		const self = this;
+		let hash = 'all';
+		let propertiesObj: stringObj | string[] = {}
 
-		selector	= self.removeMultiWhiteSpace( selector );
-		properties	= self.removeMultiWhiteSpace( properties );
+		selector = self.removeMultiWhiteSpace(selector);
+		properties = self.removeMultiWhiteSpace(properties);
 
-		if ( responsive && typeof responsive === 'object' ) {
-			hash = self.responsiveToHash( responsive );
+		if (responsive && typeof responsive === 'object') {
+			hash = self.responsiveToHash(responsive);
 		}
 
-		if ( !self.rules[ hash ] ) {
-			self.rules[ hash ] = {}
+		if (!self.rules[hash]) {
+			self.rules[hash] = {}
 		}
-		
-		if ( ! properties && selector ) {
-			const parsedRules = selector.match( /[^{]+\{[^}]+}/g );
-			if( ! parsedRules ) return;
-			for( let i in parsedRules ) {
-				const parsedRule = parsedRules[i].match( /([^{]+)\{([^}]+)}/ );
-				if ( parsedRule ) {
-					const _selector 	= parsedRule[ 1 ];
-					const _properties 	= parsedRule[ 2 ];
-					self.addRule( _selector, _properties, responsive );
+
+		if (!properties && selector) {
+			const parsedRules = selector.match(/[^{]+\{[^}]+}/g);
+			if (!parsedRules) return;
+			for (let i in parsedRules) {
+				const parsedRule = parsedRules[i].match(/([^{]+)\{([^}]+)}/);
+				if (parsedRule) {
+					const _selector = parsedRule[1];
+					const _properties = parsedRule[2];
+					self.addRule(_selector, _properties, responsive);
 				}
 			}
 			return;
 		}
 
-		if ( !self.rules[ hash ][ selector ] ) {
-			self.rules[ hash ][ selector ] = {};
+		if (!self.rules[hash][selector]) {
+			self.rules[hash][selector] = {};
 		}
-        
-		if ( 'string' === typeof properties ) {
-			propertiesObj  = properties.split( ';' ).filter( String );
+
+		if ('string' === typeof properties) {
+			propertiesObj = properties.split(';').filter(String);
 			const orderedRules: stringObj = {};
 			try {
-                let i: keyof typeof propertiesObj
-				for( i in propertiesObj ) {
-					const [ property, value ] = propertiesObj[i].split( /:(.*)?/ );
-					orderedRules[ property.trim() ] = value.trim().replace( ';', '' );
+				let i: keyof typeof propertiesObj
+				for (i in propertiesObj) {
+					const [property, value] = propertiesObj[i].split(/:(.*)?/);
+					orderedRules[property.trim()] = value.trim().replace(';', '');
 				}
-			} catch ( error ) {
+			} catch (error) {
 				return;
 			}
 
 			propertiesObj = orderedRules;
 		}
 
-		Object.assign( self.rules[ hash ][ selector ], propertiesObj );
+		Object.assign(self.rules[hash][selector], propertiesObj);
 	}
 
+	public clear(): void {
+		this.rules = {};
+		this.css = {};
+	}
+
+	public output(): string {
+
+		const { css, convertRules, createResponsiveFormat, sortHashes } = this;
+		const rules = sortHashes.call(this);
+
+		let text = '';
+		for (let hash in rules) {
+
+			let screenText = convertRules.call(this, rules[hash]);
+
+			if ('all' !== hash) {
+				screenText = createResponsiveFormat.call(this, hash) + '{' + screenText + '}';
+			}
+
+			text += screenText;
+		}
+
+		for (let i in css) {
+			text += css[i];
+		}
+
+		return text;
+	};
+
 }
-
-const sheet = new CSSMaker();
-
-sheet.addDevice('laptop', 991)
-sheet.addDevice('mobile', 575)
-sheet.addDevice('tablet', 768)
-
-sheet.addRule( '.element-a', "background: green", {max: 'laptop', min: 'mobile'} )
-sheet.addRule( '.element-a', "background: yellow", {max: 'mobile'} )
-sheet.addRule( '.element-a', "background: purple" )
-
-console.log(sheet)
